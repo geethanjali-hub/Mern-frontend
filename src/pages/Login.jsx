8794bf7297ebb72c98e4dd41e16aeaecc5d96284
geethanjali-hub <ugeethanjali5@gmail.com>
2025-12-05 17:04:53 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -26,7 +26,7 @@ const Login = () => {
         setError("");
 
         try {
-            const response = await fetch("http://localhost:5000/api/auth/login", {
+            const response = await fetch("https://mern-backend-ony8.onrender.com/api/auth/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
