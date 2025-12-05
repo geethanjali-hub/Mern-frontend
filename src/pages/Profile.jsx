f6c7070976aaeadc84c1042ba6056bce7a28dc82
geethanjali-hub <ugeethanjali5@gmail.com>
2025-12-05 17:04:31 +0530

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -16,7 +16,7 @@ const Profile = () => {
 
     const fetchProfile = async () => {
         try {
-            const response = await fetch("http://localhost:5000/api/user/profile", {
+            const response = await fetch("https://mern-backend-ony8.onrender.com/api/user/profile", {
                 headers: {
                     Authorization: `Bearer ${token}`,
                 },
