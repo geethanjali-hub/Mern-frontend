5a5ebb0acd7a9db28666345a6469fd5ec8242ec1
geethanjali-hub <ugeethanjali5@gmail.com>
2025-12-05 16:58:04 +0530

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -28,7 +28,7 @@ const Signup = () => {
         setSuccess("");
 
         try {
-            const response = await fetch("http://localhost:5000/api/auth/signup", {
+            const response = await fetch("https://mern-backend-ony8.onrender.com/api/auth/signup", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
